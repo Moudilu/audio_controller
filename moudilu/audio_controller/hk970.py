fee0e62a0cd38dceb425be0a30dee5c214450413
Moudilu <fabian.tech@ulmer.ch>
2025-02-09 21:52:34 +0100

    Add delay before power off of HK970

--- moudilu/audio_controller/hk970.py
+++ moudilu/audio_controller/hk970.py
@@ -1,5 +1,8 @@
-from lirc import Client
+from asyncio import TimerHandle, get_running_loop
 from logging import getLogger
+from typing import Union
+
+from lirc import Client
 
 from .event_router import Event, router
 
@@ -11,6 +14,11 @@ class HK970:
     Amp is controlled with IR commands issued with Lirc.
     """
 
+    _shutdown_timer: Union[TimerHandle, None] = None
+    """Timer which schedules delayed powering off of the amp"""
+    SHUTDOWN_DELAY: float = 60
+    """Delay in seconds to wait before turning amp off after playback stops"""
+
     def __init__(self) -> None:
         self._lirc = Client()
 
@@ -18,12 +26,20 @@ class HK970:
 
         self._logger = getLogger("HK970")
 
+        # Start in a well defined state
+        # If stream is currently running, amp will be restarted soon after by an event
+        self.power_off()
+
     def process_events(self, event: Event, caller: str) -> None:
         match event:
             case Event.PLAYBACK_START:
+                if self._shutdown_timer is not None:
+                    self._shutdown_timer.cancel()
                 self.power_on()
             case Event.PLAYBACK_STOP:
-                self.power_off()
+                self._shutdown_timer = get_running_loop().call_later(
+                    self.SHUTDOWN_DELAY, self.power_off
+                )
             case _:
                 pass
 
